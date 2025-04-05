cb0624be82176225246b888be3be2078560effe2
Priyambada Mohapatra <priyambadamohapatra02@gmail.com>
2025-04-05 09:55:22 +0530

    small project 1 BMI calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,5 @@
+height=int(input("enter your height"))
+weight=int(input("enter your weight in kg:"))
+bmi=weight/(height ** 2)
+print(" your bmi is "+ str(bmi))
+#replicate https://www.calculator.net/bmi-calculator this site 
